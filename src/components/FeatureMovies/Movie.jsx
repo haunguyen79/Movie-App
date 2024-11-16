15dc6c9248de2923ee35a52a397f4149daec47fb
Hunter Nguyen <Hauhonguyen2001@gmail.com>
2024-11-17 03:43:03 +0700

    Load dữ liệu từ API cho FeatureMovies

--- src/components/FeatureMovies/Movie.jsx
+++ src/components/FeatureMovies/Movie.jsx
@@ -1,30 +1,31 @@
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Movie = () => {
+const Movie = (props) => {
+  console.log({ props });
+  // const { data: { backdrop_path, title, release_date, overview }  } = props;
+  
+  // console.log(props.data?.backdrop_path);
+
   return (
     <div>
       <img
-        src="https://www.furiosaamadmaxsaga.com/images/share.jpg"
+        src={`https://image.tmdb.org/t/p/original${props.data?.backdrop_path}`}
         className="aspect-ratio h-full w-full object-cover brightness-50"
       />
       <div className="absolute bottom-[10%] left-8 w-1/2 sm:w-1/3">
-        <p className="mb-2 font-bold sm:text-[2vw]">Inside Out 2</p>
+        <p className="mb-2 font-bold sm:text-[2vw]">{props.data?.title}</p>
         <div>
-          <p className="mb-1 inline-block border border-gray-400 text-gray-400">
+          {/* {<p className="mb-1 inline-block border border-gray-400 text-gray-400">
             PG13
-          </p>
-          <p className="text-[1.2vw]">2024-10-26</p>
+          </p>} */}
+          <p className="text-[1.2vw]">{props.data?.release_date}</p>
         </div>
         <div>
           <div className="mt-4 hidden text-[1.2vw] sm:block">
             <p className="mb-2 font-bold">Overview</p>
             <p>
-              Teenager Riley&apos;s mind headquarters is undergoing a sudden
-              demolition to make room for something entirely unexpected: new
-              Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been
-              running a successful operation by all accounts, aren’t sure how to
-              feel when Anxiety shows up. And it looks like she’s not alone.
+              {props.data?.overview}
             </p>
           </div>
           <div className="mt-4">
--- src/components/FeatureMovies/index.jsx
+++ src/components/FeatureMovies/index.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import Movie from "./Movie";
 import PaginateIndicator from "./PaginateIndicator";
 
+
+//API Read Access Token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTlhYmI5NTI3M2IwNGE3MDc5YTQ1ODMyOWNmZmE4NCIsIm5iZiI6MTczMDI0OTkxNy40MjAxNDM2LCJzdWIiOiI2NzIxODM0NjgyNmZlNTc5OWNjNGExOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zeTMNZgeFRbZ1q3UAHn-g7tlxwYpm53P1Fhn4spjQmY
+
 const FeatureMovies = () => {
+  const [movies, setMovies] = useState([]);
+  // console.log("Rendering");
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/popular", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTlhYmI5NTI3M2IwNGE3MDc5YTQ1ODMyOWNmZmE4NCIsIm5iZiI6MTczMDI0OTkxNy40MjAxNDM2LCJzdWIiOiI2NzIxODM0NjgyNmZlNTc5OWNjNGExOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zeTMNZgeFRbZ1q3UAHn-g7tlxwYpm53P1Fhn4spjQmY",
+      },
+    }).then(async (res) => {
+      const data = await res.json();
+      console.log({ data });
+      setMovies(data.results);
+    });
+  }, []);
+
+  console.log(movies)
+
   return (
     <div className="relative text-white">
-      <Movie />
+      <Movie data={movies[15]}/>
       <PaginateIndicator />
     </div>
   );
