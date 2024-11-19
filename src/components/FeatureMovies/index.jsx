50948c9cc9ee6192fe424f4eb5ab26822a1e0251
Hunter Nguyen <Hauhonguyen2001@gmail.com>
2024-11-19 09:50:24 +0700

    Xây dựng Slider

--- src/components/FeatureMovies/index.jsx
+++ src/components/FeatureMovies/index.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import Movie from "./Movie";
 import PaginateIndicator from "./PaginateIndicator";
 
-
 //API Read Access Token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTlhYmI5NTI3M2IwNGE3MDc5YTQ1ODMyOWNmZmE4NCIsIm5iZiI6MTczMDI0OTkxNy40MjAxNDM2LCJzdWIiOiI2NzIxODM0NjgyNmZlNTc5OWNjNGExOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zeTMNZgeFRbZ1q3UAHn-g7tlxwYpm53P1Fhn4spjQmY
 
 const FeatureMovies = () => {
   const [movies, setMovies] = useState([]);
+  const [activeMovieId, setActiveMovieId] = useState();
   // console.log("Rendering");
 
   useEffect(() => {
@@ -20,16 +20,26 @@ const FeatureMovies = () => {
     }).then(async (res) => {
       const data = await res.json();
       console.log({ data });
-      setMovies(data.results);
+      const popularMovies = data.results.slice(0, 4);
+      setMovies(popularMovies);
+      setActiveMovieId(popularMovies[0].id);
     });
   }, []);
 
-  console.log(movies)
+  console.log(movies);
 
   return (
     <div className="relative text-white">
-      <Movie data={movies[15]}/>
-      <PaginateIndicator />
+      {movies
+        .filter((movie) => movie.id === activeMovieId)
+        .map((movie) => (
+          <Movie key={movie.id} data={movie} />
+        ))}
+      <PaginateIndicator
+        movies={movies}
+        activeMovieId={activeMovieId}
+        setActiveMovieId={setActiveMovieId}
+      />
     </div>
   );
 };
