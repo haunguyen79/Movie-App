21dbfe35be05bd291570ff0d80bad06d7d49e3e6
Hunter Nguyen <Hauhonguyen2001@gmail.com>
2024-11-23 21:41:00 +0700

    Tái sử dụng Component để xây dựng Top Rated

--- src/components/MediaList/index.jsx
+++ src/components/MediaList/index.jsx
@@ -1,49 +1,38 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
-const TABS = [
-  {
-    id: "all",
-    name: "All",
-  },
-  {
-    id: "movie",
-    name: "Movie",
-  },
-  {
-    id: "tv",
-    name: "TV Show",
-  },
-];
-
-const MediaList = () => {
+const MediaList = ({ title, tabs }) => {
   const [mediaList, setMediaList] = useState([]);
-  const [activeTabId, setActiveTabId] = useState(TABS[0].id);
+  const [activeTabId, setActiveTabId] = useState(tabs[0]?.id);
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/trending/${activeTabId}/day`, {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTlhYmI5NTI3M2IwNGE3MDc5YTQ1ODMyOWNmZmE4NCIsIm5iZiI6MTczMDI0OTkxNy40MjAxNDM2LCJzdWIiOiI2NzIxODM0NjgyNmZlNTc5OWNjNGExOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zeTMNZgeFRbZ1q3UAHn-g7tlxwYpm53P1Fhn4spjQmY",
-      },
-    }).then(async (res) => {
-      const data = await res.json();
-      console.log({ data });
+    const url = tabs.find((tab) => tab.id === activeTabId)?.url;
 
-      const trendingMediaList = data.results.splice(0, 12);
-      console.log(trendingMediaList);
-      setMediaList(trendingMediaList);
-    });
-  }, [activeTabId]);
+    if (url) {
+      fetch(url, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTlhYmI5NTI3M2IwNGE3MDc5YTQ1ODMyOWNmZmE4NCIsIm5iZiI6MTczMDI0OTkxNy40MjAxNDM2LCJzdWIiOiI2NzIxODM0NjgyNmZlNTc5OWNjNGExOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zeTMNZgeFRbZ1q3UAHn-g7tlxwYpm53P1Fhn4spjQmY",
+        },
+      }).then(async (res) => {
+        const data = await res.json();
+        console.log({ data });
+
+        const trendingMediaList = data.results.splice(0, 12);
+        console.log(trendingMediaList);
+        setMediaList(trendingMediaList);
+      });
+    }
+  }, [activeTabId, tabs]);
 
   return (
     <div className="bg-black px-8 py-10 text-[1.2vw] text-white">
       <div className="mb-6 flex items-center gap-4">
-        <p className="text-[2vw] font-bold">Trending</p>
+        <p className="text-[2vw] font-bold">{title}</p>
         <ul className="flex rounded border border-white">
-          {TABS.map((tab) => (
+          {tabs.map((tab) => (
             <li
               key={tab.id}
               className={`cursor-pointer rounded px-2 py-1 ${tab.id === activeTabId ? `bg-white text-black` : ``}`}
@@ -69,7 +58,7 @@ const MediaList = () => {
             releaseDay={media.release_date || media.first_air_date}
             poster={media.poster_path}
             point={media.vote_average}
-            mediaType={media.media_type}
+            mediaType={media.media_type || activeTabId}
           />
         ))}
       </div>
