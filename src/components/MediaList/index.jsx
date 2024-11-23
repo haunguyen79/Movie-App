6598d5bb9217b512f57a027653413cf2afc6040f
Hunter Nguyen <Hauhonguyen2001@gmail.com>
2024-11-23 12:01:52 +0700

    Load dữ liệu từ API cho MovieCard

--- src/components/MediaList/index.jsx
+++ src/components/MediaList/index.jsx
@@ -1,11 +1,27 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
+const TABS = [
+  {
+    id: "all",
+    name: "All",
+  },
+  {
+    id: "movie",
+    name: "Movie",
+  },
+  {
+    id: "tv",
+    name: "TV Show",
+  },
+];
+
 const MediaList = () => {
   const [mediaList, setMediaList] = useState([]);
+  const [activeTabId, setActiveTabId] = useState(TABS[0].id);
 
   useEffect(() => {
-    fetch("https://api.themoviedb.org/3/trending/all/day", {
+    fetch(`https://api.themoviedb.org/3/trending/${activeTabId}/day`, {
       method: "GET",
       headers: {
         accept: "application/json",
@@ -20,18 +36,28 @@ const MediaList = () => {
       console.log(trendingMediaList);
       setMediaList(trendingMediaList);
     });
-  }, []);
+  }, [activeTabId]);
 
   return (
     <div className="bg-black px-8 py-10 text-[1.2vw] text-white">
       <div className="mb-6 flex items-center gap-4">
         <p className="text-[2vw] font-bold">Trending</p>
         <ul className="flex rounded border border-white">
-          <li className="cursor-pointer rounded bg-white px-2 py-1 text-black">
+          {TABS.map((tab) => (
+            <li
+              key={tab.id}
+              className={`cursor-pointer rounded px-2 py-1 ${tab.id === activeTabId ? `bg-white text-black` : ``}`}
+              onClick={() => setActiveTabId(tab.id)}
+            >
+              {tab.name}
+            </li>
+          ))}
+
+          {/* <li className="cursor-pointer rounded bg-white px-2 py-1 text-black">
             All
           </li>
           <li className="cursor-pointer rounded px-2 py-1">Movie</li>
-          <li className="cursor-pointer rounded px-2 py-1">TV-Show</li>
+          <li className="cursor-pointer rounded px-2 py-1">TV-Show</li> */}
         </ul>
       </div>
 
