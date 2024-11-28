9c149e3e1cf06b9d77b2a2f79ad3262f0e443b9c
Hunter Nguyen <Hauhonguyen2001@gmail.com>
2024-11-28 15:01:27 +0700

    Cải thiện UX với hiệu ứng Loading

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -4,13 +4,16 @@ import CircularProgressBar from "../components/CircularProgressBar";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { groupBy } from "lodash";
+import Loading from "../components/Loading";
 
 const MovieDetail = () => {
   const { id } = useParams(); // Lấy tham số userId từ URL
   const [movieInfo, setMovieInfo] = useState({});
+  const [isLoading, setIsloading] = useState(false);
   // console.log({ params });
 
   useEffect(() => {
+    setIsloading(true);
     fetch(
       `https://api.themoviedb.org/3/movie/${id}?append_to_response=release_dates,credits`,
       {
@@ -21,11 +24,18 @@ const MovieDetail = () => {
             "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTlhYmI5NTI3M2IwNGE3MDc5YTQ1ODMyOWNmZmE4NCIsIm5iZiI6MTczMDI0OTkxNy40MjAxNDM2LCJzdWIiOiI2NzIxODM0NjgyNmZlNTc5OWNjNGExOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zeTMNZgeFRbZ1q3UAHn-g7tlxwYpm53P1Fhn4spjQmY",
         },
       },
-    ).then(async (res) => {
-      const data = await res.json();
-      console.log({ data });
-      setMovieInfo(data);
-    });
+    )
+      .then(async (res) => {
+        const data = await res.json();
+        console.log({ data });
+        setMovieInfo(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setIsloading(false);
+      });
   }, [id]);
 
   const certification = (
@@ -42,6 +52,12 @@ const MovieDetail = () => {
 
   const groupedCrews = groupBy(crews, "job");
   console.log({ crews, groupedCrews });
+
+  if(isLoading){
+  return (
+    <Loading />
+  );
+  }
 
   return (
     <div className="relative overflow-hidden text-white">
