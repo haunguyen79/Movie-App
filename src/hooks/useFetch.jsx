40ab0fd881a0388c6aeb8878590bb40dffbddded
Hunter Nguyen <Hauhonguyen2001@gmail.com>
2024-12-01 11:35:52 +0700

    Tạo custom hook đầu tiên - useFetch

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+const DEFAULT_HEADERS = {
+  accept: "application/json",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTlhYmI5NTI3M2IwNGE3MDc5YTQ1ODMyOWNmZmE4NCIsIm5iZiI6MTczMDI0OTkxNy40MjAxNDM2LCJzdWIiOiI2NzIxODM0NjgyNmZlNTc5OWNjNGExOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zeTMNZgeFRbZ1q3UAHn-g7tlxwYpm53P1Fhn4spjQmY",
+};
+
+export default function useFetch({ url = "", method = "GET", headers = {} }) {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`${import.meta.env.VITE_API_HOST}${url}`, {
+      method,
+      headers: {
+        ...DEFAULT_HEADERS,
+        ...headers,
+      },
+    })
+      .then(async (res) => {
+        const data = await res.json();
+        // console.log({ data });
+        setData(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url, method, JSON.stringify(headers)]);
+
+  return {isLoading, data};
+}
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -5,41 +5,48 @@ import Banner from "@components/MediaDetail/Banner";
 import ActorList from "@components/MediaDetail/ActorList";
 import RelatedMediaList from "@components/MediaDetail/RelatedMediaList";
 import MovieInformation from "@components/MediaDetail/MovieInformation";
+import useFetch from "@hooks/useFetch";
 
 const MovieDetail = () => {
   const { id } = useParams(); // Lấy tham số userId từ URL
-  const [movieInfo, setMovieInfo] = useState({});
+  // const [movieInfo, setMovieInfo] = useState({});
   const [relatedMovie, setRelatedMovie] = useState([]);
-  const [isLoading, setIsloading] = useState(false);
+  // const [isLoading, setIsloading] = useState(false);
   // const [isRelatedMovieListLoading, setIsRelatedMovieListLoading] =
   //   useState(false);
   // console.log({ params });
 
-  useEffect(() => {
-    setIsloading(true);
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?append_to_response=release_dates,credits`,
-      {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTlhYmI5NTI3M2IwNGE3MDc5YTQ1ODMyOWNmZmE4NCIsIm5iZiI6MTczMDI0OTkxNy40MjAxNDM2LCJzdWIiOiI2NzIxODM0NjgyNmZlNTc5OWNjNGExOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zeTMNZgeFRbZ1q3UAHn-g7tlxwYpm53P1Fhn4spjQmY",
-        },
-      },
-    )
-      .then(async (res) => {
-        const data = await res.json();
-        console.log({ data });
-        setMovieInfo(data);
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-      .finally(() => {
-        setIsloading(false);
-      });
-  }, [id]);
+  // useEffect(() => {
+  //   setIsloading(true);
+  //   fetch(
+  //     `https://api.themoviedb.org/3/movie/${id}?append_to_response=release_dates,credits`,
+  //     {
+  //       method: "GET",
+  //       headers: {
+  //         accept: "application/json",
+  //         Authorization:
+  //           "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTlhYmI5NTI3M2IwNGE3MDc5YTQ1ODMyOWNmZmE4NCIsIm5iZiI6MTczMDI0OTkxNy40MjAxNDM2LCJzdWIiOiI2NzIxODM0NjgyNmZlNTc5OWNjNGExOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zeTMNZgeFRbZ1q3UAHn-g7tlxwYpm53P1Fhn4spjQmY",
+  //       },
+  //     },
+  //   )
+  //     .then(async (res) => {
+  //       const data = await res.json();
+  //       console.log({ data });
+  //       setMovieInfo(data);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     })
+  //     .finally(() => {
+  //       setIsloading(false);
+  //     });
+  // }, [id]);
+
+  const { data: movieInfo, isLoading } = useFetch({
+    url: `/movie/${id}?append_to_response=release_dates,credits`,
+  });
+
+  console.log({ movieInfo, isLoading });
 
   useEffect(() => {
     // setIsRelatedMovieListLoading(true);
