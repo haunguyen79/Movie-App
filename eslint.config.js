79201570e1443b90afad6ab54f26e0ed247dd7bc
Hunter Nguyen <Hauhonguyen2001@gmail.com>
2025-06-03 22:43:28 +0700

    Updated Title Name

--- eslint.config.js
+++ eslint.config.js
@@ -30,11 +30,6 @@ export default [
       ...reactHooks.configs.recommended.rules,
       "react/jsx-no-target-blank": "off",
       "react/prop-types": "off",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "react/prop-types": "off",
       "react-refresh/only-export-components": "off",
     },
   },
